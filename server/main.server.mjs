d68edeec63281a6d6413037f7f17470060d764f7
fatma index <fgmal019@gmail.com>
2024-12-16 16:06:37 +0100

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,11 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  AppServerModule
-} from "./chunk-3JA4MTMK.mjs";
-import "./chunk-3IUATXN4.mjs";
-import "./chunk-QGL4KPPF.mjs";
-import "./chunk-24VIC3GD.mjs";
-export {
-  AppServerModule as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-2OOVZIOR.mjs";import"./chunk-AL6A4ZIX.mjs";import"./chunk-RYYKCM6L.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
